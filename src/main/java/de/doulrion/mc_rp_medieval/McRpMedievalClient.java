aef45459e0314d3d33a5d7c89ff7b70e1a122942
Jan Hoegerle <jan.hoegerle@gmail.com>
2024-08-25 14:23:51 +0200

    Initial commit

---
+++ src/main/java/de/doulrion/mc_rp_medieval/McRpMedievalClient.java
@@ -0,0 +1,10 @@
+package de.doulrion.mc_rp_medieval;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class McRpMedievalClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/de/doulrion/mc_rp_medieval/McRpMedievalDataGenerator.java
@@ -0,0 +1,11 @@
+package de.doulrion.mc_rp_medieval;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class McRpMedievalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
